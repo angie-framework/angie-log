5fb9bafc1a9bbf2eaec721d251150933c92ed80d
jgrosecl49 <joe.groseclose@ordergroove.com>
2015-08-30 15:52:07 -0400

    Added bread to gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,7 +10,9 @@ import mocha from               'gulp-mocha';
 import cobertura from           'istanbul-cobertura-badger';
 import babel from               'gulp-babel';
 import esdoc from               'gulp-esdoc';
-import chalk from               'chalk';
+import {bold, red} from             'chalk';
+
+const bread = (str) => bold(red(str));
 
 const SRC_DIR = 'src',
     SRC = `${SRC}/**/*.js`,
